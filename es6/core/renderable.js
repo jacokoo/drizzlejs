a4f7bc68d70eb1b260cc59130729f8ab23d85f45
guyong <jaco.koo@guyong.in>
2015-11-30 03:39:39 +0800

    draft

--- es6/core/renderable.js
+++ es6/core/renderable.js
@@ -3,23 +3,22 @@ D.Renderable = class Renderable extends D.Base {
         super(name, options, {
             app: app,
             module: mod,
+            components: {},
             _loader: loader,
-            _components: {}
+            _componentMap: {},
+            _events: {},
+            _eventHandlers: this._option('handlers')
         });
 
         app.delegateEvent(this);
     }
 
     initialize () {
-        this._loadedPromise = app.templateEngine._load(this);
+        this._loadedPromise = this.chain([app.templateEngine._load(this), this._initializeEvents()]);
     }
 
-    get _element {
-        return this._region ? this._region.getElement(this) : null;
-    }
-
-    _wrapDomId (id) {
-        return this.id + id;
+    render (options) {
+        return this._render(options, true);
     }
 
     $ (id) {
@@ -30,16 +29,112 @@ D.Renderable = class Renderable extends D.Base {
         return this._element.querySelectorAll(selector);
     }
 
+    _render (options = {}, update) {
+        if (!this.region) this.error('Region is null');
+
+        this.renderOptions = options;
+        return this.chain(
+            this._loadedPromise,
+            this._destroyComponents,
+            () => this._option('beforeRender'),
+            this._beforeRender,
+            this._serializeData,
+            this._renderTemplate,
+            this._renderComponents,
+            this._afterRender,
+            () => this._option('afterRender'),
+            this
+        );
+    }
+
     _setRegion (region) {
         this._region = region;
+        this._bindEvents();
     }
 
-    render (options) {
-        this.renderOptions = options;
+    _close () {
+        this._unbindEvents();
     }
 
-    beforeRender () {}
-    afterRender () {}
-    beforeClose () {}
-    afterClose () {}
+    get _element {
+        return this._region ? this._region.getElement(this) : null;
+    }
+
+    _serializeData () {
+        return {
+            Global: this.app.global,
+            Self: this
+        };
+    }
+
+    _renderTemplate (data) {
+
+    }
+
+    _initializeEvents (events) {
+        mapObj(events || this._option('events'), (value, key) => {
+            let items = key.replace(/(^\s+)|(\s+$)/g, '').split(/\s+/), result = {key: key};
+
+            if (items.length !== 2) this._error('Invalid event key');
+            result.eventType = items[0];
+            if (item[1].slice(-1) === '*') {
+                result.id = this._wrapDomId(items[1].slice(0, -1));
+                result.haveStar = true;
+                result.selector = `[id^=${result.id}]`;
+            } else {
+                result.id = this._wrapDomId(items[1]);
+                result.selector = `#${result.id}`
+            }
+            result.handler = this._createEventHandler(value, result);
+            this._events[key] = result;
+        });
+    }
+
+    _createEventHandler (handlerName, {haveStar, id}) {
+        let {disabledClass} = this.app.options;
+        return (event) => {
+            if (!this._eventHandlers[handlerName]) this._error('No event handler for name:', handlerName);
+
+            let target = D.Adapter.getEventTarget(event), args = [event];
+            if (D.Adapter.hasClass(target, disabledClass)) return;
+            if (haveStar) args.unshift(target.getAttribute('id').slice(id.length));
+            this._eventHandlers[handlerName].apply(this, args);
+        }
+    }
+
+    _bindEvents () {
+        mapObj(this._events, (value) => {
+            this._region._delegateDomEvent(this, value.eventType, value.selector, value.handler);
+        });
+    }
+
+    _unbindEvents () {
+        this._region._undelegateDomEvents(this);
+    }
+
+    _renderComponents () {
+        this.chain(map(this._option('components'), (item) => {
+            if (D.isFunction(item)) item = item.call(this);
+            return item ? D.ComponentManager.create(this, item) : null;
+        }), (components) => map(components, (item) => {
+            if (!item) return;
+            let {id, component, index} = item, value = this.components[id];
+            D.isArray(value) ? value.push(component) : (this.components[id] = value ? [value, component] : component);
+            this._componentMap[index] = item;
+        }));
+    }
+
+    _destroyComponents () {
+        this.components = {};
+        mapObj(this._componentMap, (value) => D.ComponentManager.destroy(this, value))
+    }
+
+    _wrapDomId (id) {
+        return this.id + id;
+    }
+
+    _beforeRender () {}
+    _afterRender () {}
+    _beforeClose () {}
+    _afterClose () {}
 };
--- es6/util/adapter.js
+++ es6/util/adapter.js
@@ -1,12 +1,11 @@
 D.Adapter = {
     Promise: Promise,
 
-    ajax (params) {
-        throw new Error('Ajax is not implemented');
-    },
+    ajax (params) { throw new Error('Ajax is not implemented'); },
 
-    ajaxResult (args) {
-        return args[0];
-    }
+    ajaxResult (args) { return args[0]; },
+
+    getEventTarget (event) { return e.currentTarget || e.target; }
+
 
 };
