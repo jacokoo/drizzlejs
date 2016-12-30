7b688094c28a3a5ff14a121fad973cc19bfd8f10
guyong <jaco.koo@guyong.in>
2016-12-30 10:08:39 +0800

    add missed files

--- src/drizzle.combined.js
+++ src/drizzle.combined.js
@@ -1110,7 +1110,7 @@ D.Model = function Model (store, options) {
         store
     });
 
-    this.data = clone(this._option('data'));
+    this.data = clone(this._option('data')) || {};
     this._idKey = this._option('idKey') || this.app.options.idKey;
     this.params = assign({}, this._option('params'));
     this.app.delegateEvent(this);
